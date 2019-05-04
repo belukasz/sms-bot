b53580d106377bed166ffae48b0af6f3e1c55fe2
Lukasz Bombol <lbombol@gmail.com>
2019-05-04 21:58:26 +0200

    spring boot project set up

---
+++ src/main/java/org/belukasz/foos/smsbot/SmsBotApplication.java
@@ -0,0 +1,13 @@
+package org.belukasz.foos.smsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsBotApplication.class, args);
+	}
+
+}
